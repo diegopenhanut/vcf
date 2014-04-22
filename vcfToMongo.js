7b2339889b5255d493566c7ac08be2229dfd7063
diegopenhanut <diegopenhanut@gmail.com>
2014-04-22 11:23:40 -0500

    First commit: vcfToMongo.js is a version of vcf without the UI. It does the parsing of vcf files so resulting objects cold be handled in console by using yourVariableHere=VCFparse(vcfTxt);

---
+++ vcfToMongo.js
@@ -0,0 +1,91 @@
+// http://www.javascripture.com/FileReade
+var vcfTxt = "anything";
+var openFile = function(event) {
+	//var vcf = new VCF();
+	//console.log(this.vcfTxt);
+
+var input = event.target;
+	var reader = new FileReader();
+	reader.onload = function(event){
+	var reader = event.target;
+	
+	vcfTxt = reader.result;
+	console.log(reader.result.substring(0, 100));
+		};
+	reader.readAsText(input.files[0]);
+	VCFparse(vcfTxt);
+
+
+}
+	//to continue with same code
+	
+	
+	VCFparse=function(x){
+	console.log('(parsing a '+x.length+' long string)');
+	x=x.split(/\n/);
+	var n=x.length; // number of lines in the file
+	if(x[n-1].length==0){n=n-1}; // remove trailing blank
+	y={head:{},body:{}};
+	// parse ## head lines
+	var i=0; // ith line
+	var L = x[i].match(/^##(.*)/); // L is the line being parsed
+	if(L==null){
+		throw(x[i]);
+	}
+	
+	while(L.length>1){
+		i++;
+		L = L[1].match(/([^=]+)\=(.*)/);
+		if(!y.head[L[1]]){y.head[L[1]]=[]}
+		y.head[L[1]].push(L[2]);
+		L = x[i].match(/^##(.*)/);
+		if(L==null){L=[]}; // break	
+	}
+	// parse # body lines
+	L=x[i].match(/^#([^#].*)/)[1]; // use fuirst line to define fields
+	var F = L.split(/\t/); // fields
+	for(var j=0;j<F.length;j++){
+		y.body[F[j]]=[];
+	}
+	var i0=i+1;
+	for(var i=i0;i<n;i++){
+		L = x[i].split(/\t/);
+		for(var j=0;j<F.length;j++){
+			y.body[F[j]][i-i0]=L[j];
+		}	
+	}
+	y.fields=F;
+	VCFparseHead(y); // parse head further
+	return y;
+};
+
+VCFparseHead=function(dt){ // go through a data file and parses data.head
+	var fields = Object.getOwnPropertyNames(dt.head);
+	var newHead={}; // parse old head into here
+	var f, v, str, ID; // place holder for fields, their values, the string line, and IDs during parsing
+	var AV, AVk; // attribute=value pairs during parsing of array fields
+	for(var i=0;i<fields.length;i++){
+		//ID=str.match(/ID=([^\,\>]+)/)[1];
+		//dt.head.INFO[ID]={
+		// array entries are pushed with <> entries
+		f = fields[i];
+		if(dt.head[f][0][0]!='<'){ // the non array head fields
+			dt.head[f]=dt.head[f][0];
+		} else { // the array head fields
+			v={};
+			for(j=0;j<dt.head[f].length;j++){
+				str=dt.head[f][j];
+				ID=str.match(/ID=([^\,\>]+)/)[1];
+				v[ID]={};
+				AV = str.match(/([^\,\<]+=[^\,\>]+)/g);
+				for(k=1;k<AV.length;k++){ // k=0 is if ID's AV
+					AVk=AV[k].match(/[^=\"]+/g);
+					v[ID][AVk[0]]=AVk[1];
+				}
+			}
+			dt.head[f]=v;
+		}
+	};
+	// return dt <-- no need, dt was passed by reference
+	
+};
